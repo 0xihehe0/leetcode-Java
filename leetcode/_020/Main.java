c74f47dd1a7a76765170447d567add149f1bf1b8
yaojinxi <864554492@qq.com>
2024-01-04 23:45:26 +0000

    creat 020

---
+++ leetcode/_020/Main.java
@@ -0,0 +1,42 @@
+package _020;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution so = new Solution();
+
+        System.out.println(so.isValid("(}{)"));
+    }
+}
+
+class Solution {
+    public boolean isValid(String s) {
+        boolean checkStatus = false;
+        if(s.length() % 2 == 1){
+            return checkStatus;
+        }
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+                if(stack.isEmpty() || map.get(c) != stack.peek()){
+                    return checkStatus;
+                }
+                stack.pop();
+            }else{
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
